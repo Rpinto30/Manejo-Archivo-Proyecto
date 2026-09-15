381c24b929ad1de5bd211e860ade3c26708b5d4e
Rpinto30 <106196765+Rpinto30@users.noreply.github.com>
2026-09-15 11:40:58 -0600

    archivos para el cambio de idioma

---
+++ translations/app_en.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>MainWindow</name>
+    <message><source>MainWindow</source><translation>MainWindow</translation></message>
+    <message><source>Nuevo </source><translation>New</translation></message>
+    <message><source>Abrir</source><translation>Open</translation></message>
+    <message><source>Guardar</source><translation>Save</translation></message>
+    <message><source>Salir</source><translation>Exit</translation></message>
+    <message><source>Cortar</source><translation>Cut</translation></message>
+    <message><source>Pegar</source><translation>Paste</translation></message>
+    <message><source>Seleccionar Todo</source><translation>Select All</translation></message>
+    <message><source>Ver foto</source><translation>View photo</translation></message>
+    <message><source>Pantalla completa</source><translation>Full screen</translation></message>
+    <message><source>Minimizar</source><translation>Minimize</translation></message>
+    <message><source>Nombre de usuario</source><translation>Username</translation></message>
+    <message><source>Tema (Claro/Oscuro)</source><translation>Theme (Light/Dark)</translation></message>
+    <message><source>Idioma (es/es-Es, en/en-US)</source><translation>Language (es/es-Es, en/en-US)</translation></message>
+    <message><source>Barra lateral</source><translation>Sidebar</translation></message>
+    <message><source>Color de letra</source><translation>Font color</translation></message>
+    <message><source>Foto de perfil</source><translation>Profile picture</translation></message>
+    <message><source>Mostrar/Ocultar panel lateral</source><translation>Show/Hide side panel</translation></message>
+    <message><source>Inserta una imagen...</source><translation>Insert an image...</translation></message>
+    <message><source>MEMORIAS DE: </source><translation>MEMORIES BY: </translation></message>
+    <message><source>Archivo</source><translation>File</translation></message>
+    <message><source>Edición</source><translation>Edit</translation></message>
+    <message><source>Ver</source><translation>View</translation></message>
+    <message><source>Configuración</source><translation>Settings</translation></message>
+    <message><source>Opciones de Memorias</source><translation>Memories Settings</translation></message>
+    <message><source>Cargar</source><translation>Load</translation></message>
+    <message><source>Sin datos que cargar...</source><translation>No data to load...</translation></message>
+    <message><source></source><translation></translation></message>
+</context>
+</TS>
